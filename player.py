bfcf8b43a24a7cd1eb7d90beed7169e0b44560e4
Asherah C. <miss.ashtarva@gmail.com>
2026-07-21 15:02:17 -0700

    Player rotation enabled

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS,LINE_WIDTH
+from constants import PLAYER_RADIUS,LINE_WIDTH, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 import pygame
 
@@ -7,7 +7,6 @@ class Player(CircleShape):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
     
-    # in the Player class
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -19,3 +18,13 @@ class Player(CircleShape):
     def draw(self,screen):
         pygame.draw.polygon(screen,"white",self.triangle(),LINE_WIDTH)
 
+    def rotate(self,dt:float):
+        self.rotation = self.rotation + PLAYER_TURN_SPEED*dt
+
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(+dt)
